4ae67a7e0e4af325b3517b8a3492d43b3bb8aff6
Anderson Comar <andersoncomar@gmail.com>
2024-12-25 22:39:31 +0000

    Add route Auth and Organization

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,23 +1,76 @@
 import fastifyCors from '@fastify/cors'
+import fastifyJwt from '@fastify/jwt'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUI from '@fastify/swagger-ui'
+import { env } from '@saas/env'
 import { fastify } from 'fastify'
 import {
-  // jsonSchemaTransform,
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
   ZodTypeProvider,
 } from 'fastify-type-provider-zod'
 
+import { errorHandler } from './error-handler'
+import { authenticateWithGithub } from './routes/auth/authenticate-with-github'
+import { authenticateWithPassword } from './routes/auth/authenticate-with-password'
 import { createAccount } from './routes/auth/create-account'
+import { getProfile } from './routes/auth/get-profile'
+import { requestPasswordRecover } from './routes/auth/request-password-recover'
+import { resetPassword } from './routes/auth/reset-password'
+import { createOrganization } from './routes/orgs/create-organization'
+import { getMembership } from './routes/orgs/get-membership'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
 
+app.setErrorHandler(errorHandler)
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'SaaS RBAC',
+      description: 'Full-stack SaaS app with multi-tenant & RBAC',
+      version: '1.0.0',
+    },
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUI, {
+  routePrefix: '/docs',
+})
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+})
+
 app.register(fastifyCors)
 
 app.register(createAccount)
 
-app.listen({ port: 3333 }).then(() => {
-  console.log(`HTTP server running in Port 3333`)
+app.register(authenticateWithPassword)
+app.register(authenticateWithGithub)
+
+app.register(requestPasswordRecover)
+app.register(resetPassword)
+
+app.register(getProfile)
+
+app.register(createOrganization)
+app.register(getMembership)
+
+app.listen({ port: env.SERVER_PORT }).then(() => {
+  console.log(`HTTP server running in Port ${env.SERVER_PORT}`)
 })
